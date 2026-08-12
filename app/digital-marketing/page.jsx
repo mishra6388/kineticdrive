18ca44396820ecdbccb90825f34894368670d76a
kineticdrive25 <cloud@kineticdrive.in>
2026-08-12 17:15:32 +0530

    feat: add digital marketing landing page and supporting assets

--- app/digital-marketing/page.jsx
+++ app/digital-marketing/page.jsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react';
 import HeroFormSection from './_components/HeroFormSection';
-import SocialProof from '../web-development/_components/SocialProof';
+import TrustSection from '../website-design-development/_components/TrustSection';
 import WhyChoose from './_components/WhyChoose';
 import ServicesSection from './_components/ServicesSection';
 import ProcessTimeline from './_components/ProcessTimeline';
 import ToolsWeUse from './_components/ToolsWeUse';
-import TestimonialsSection from '../web-development/_components/TestimonialsSection';
+import TestimonialsSection from '../website-design-development/_components/TestimonialsSection';
 import FAQSection from './_components/FAQSection';
 import FinalCTA from './_components/FinalCTA';
 
@@ -14,7 +14,7 @@ export default function DigitalMarketingLandingPage() {
   return (
     <main className="bg-[#050508] text-white min-h-screen">
       <HeroFormSection />
-      <SocialProof />
+      <TrustSection />
       <WhyChoose />
       <ServicesSection />
       <ProcessTimeline />
